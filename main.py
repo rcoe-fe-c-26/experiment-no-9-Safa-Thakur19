1fadc806484d9f9af0be855886e49c30bb4727ee
Safa-Thakur19 <safathakur8@gmail.com>
2026-03-26 17:16:44 +0530

    Enhance main.py with author info and factorial logic
    Added author and date comments, implemented factorial calculation.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Safa Thakur
+# Date: 06/02/2026
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+n = int(input("Enter Number: "))
+
+if n < 0:
+  print(f"Factorial of {n} is Not Defined")
+else:
+  fact = 1 
+  for x in range(1, n + 1):
+    fact = fact * x
+    print(f"Factorial of {n} is {fact}")
